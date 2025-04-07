696f4d26977c0fdb51ae61467e3a0b59fffdf5b6
Starpen8Volkov <olegoon.volkov@gmail.com>
2025-04-07 10:04:41 +0930

    Fixed Collision Code

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,10 +3,17 @@ extends Node
 var score = 0
 var rawKey = preload("res://Scenes/key.tscn")
 var Player
+var tileMap
+var tileSize = Vector2(0,0)
+var solids
+var borderSize=20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Player=get_tree().get_first_node_in_group("player")
+	tileMap = get_tree().get_first_node_in_group("tilemap") 
+	tileSize = Vector2(tileMap.tile_set.tile_size)
+	solids = get_tree().get_nodes_in_group("solid")
 	
 	generateKeys(15)
 
@@ -24,8 +31,12 @@ func keyCollected():
 
 
 func generateKeys(num):
-	var i=0;
-	while(i<num):
+	for i in num:
 		var freshKey=rawKey
-		get_tree().get_first_node_in_group("map").add_child(freshKey.instantiate())
-		i+=1
+		freshKey=freshKey.instantiate()
+		get_tree().get_first_node_in_group("map").add_child(freshKey)
+		chooseRandomTile(freshKey)
+
+
+func chooseRandomTile(node):
+	node.position = ((Vector2(randi_range(0+borderSize,800-borderSize),randi_range(0+borderSize,450-borderSize))/Global.tileSize).round()*Global.tileSize)+(Global.tileSize/2)
